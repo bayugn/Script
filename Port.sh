4256c7be683909a295fbbea10206d078177c227d
Tuyul-Google <53965383+Tuyul-Google@users.noreply.github.com>
2020-09-12 02:25:44 +0700

    Update Port.sh

--- Port.sh
+++ Port.sh
@@ -7,9 +7,9 @@ clear
 function banner(){
 		echo -e "\t${p}_________________________"
 		echo -e "\t${h}  TOOLS SCANNING PROXY"
-		echo -e "\t${p}   Author${m}: ${p}KumpulanRemaja "
-		echo -e "\t${p}    KumpulanRemaja.com"
-		echo -e "\t${p}    Recode bye RusmanaID "
+		echo -e "\t${p}   Author${m}: ${p} Tuyul-Google "
+		echo -e "\t${p}    https://tuyul-google.blogspot.com"
+		echo -e "\t${p}    Recode bye RYAN CREATOR "
 		echo -e "\t${p}_________________________"
 	   	
 }
